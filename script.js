c89660c6b6df475fe13500bb05658f61092c9eb0
HildaKornman <84178454+HildaKornman@users.noreply.github.com>
2021-06-19 22:32:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//Deel 1:
+const age = 18;
+if (age>17){
+    console.log("U bent 18 jaar of ouder, welkom in deze kroeg!");
+} else {
+    console.log("U bent helaas nog te jong om u welkom te mogen heten in deze kroeg!");
+}
+//Deel 2:
+const isFemale = false;
+if (isFemale){
+    console.log("Welkom op de ladiesnight!");
+} else {
+    console.log("Helaas zijn alleen dames welkom op de ladiesnight!");
+}
+//Deel 3:
+const driverStatus='bob';
+if (driverStatus=='bob'){
+    console.log("Beste bob; We zullen uw auto voorrijden en wensen u een veilige reis naar huis!");
+} else {
+    console.log("Na zo'n mooie avond laat u zich lekker naar huis rijden; we zullen een taxi voor u bellen!");
+}
